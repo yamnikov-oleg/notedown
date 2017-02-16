af17ace0fee3dd4bf3c76e07ca587d0023942f39
Oleg Yamnikov <bgeyts667@gmail.com>
2017-02-16 13:24:04 +0500

    Basic app to create, edit and delete notes

--- web.py
+++ web.py
@@ -1,21 +1,34 @@
-from flask import Flask, request, abort
+from flask import Flask, abort, render_template, redirect, request, url_for
 
-import models
+from models import Note
 
 app = Flask(__name__)
 
-@app.route('/')
-def root():
-    text_iter = models.Note.select(models.Note.text).tuples().execute()
-    texts = []
-    for (t,) in text_iter:
-        texts.append(t)
-    return "\n".join(texts)
+@app.route('/', methods=['GET'])
+def index():
+    notes = Note.select()
+    edit_note = Note.select().where(Note.id == request.args.get('edit')).first()
+    return render_template("index.html", notes=notes, edit_note=edit_note)
 
-@app.route('/create')
-def create():
-    text = request.args.get('text')
+@app.route('/create', methods=['POST'])
+def save():
+    text = request.form.get('text')
     if not text:
-        abort(400)
-    models.Note.create(text=text)
-    return "Created:\n" + text
+        return redirect(url_for('index'))
+
+    nid = request.form.get('id')
+    if nid:
+        note = Note.get(Note.id == nid)
+
+    if not nid or not note:
+        note = Note()
+
+    note.text = text
+    note.save()
+    return redirect(url_for('index'))
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    nid = request.form.get('id')
+    Note.delete().where(Note.id == nid).execute()
+    return redirect(url_for('index'))
