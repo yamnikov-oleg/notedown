2c48294e5f906226c6e50fbed7e468056e3bb84d
Oleg Yamnikov <bgeyts667@gmail.com>
2017-02-21 18:03:28 +0500

    Implement checkboxes for markdown

--- markdown.py
+++ markdown.py
@@ -2,4 +2,6 @@ import mistune
 
 def render(md):
     rendered = mistune.markdown(md)
+    rendered = rendered.replace("[ ]", '<input type="checkbox" disabled>')
+    rendered = rendered.replace("[x]", '<input type="checkbox" disabled checked>')
     return rendered
