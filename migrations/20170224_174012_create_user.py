f116c4c4e55c1c9254dc9ec1266ec901b9a577e5
Oleg Yamnikov <bgeyts667@gmail.com>
2017-02-24 17:53:44 +0500

    Add User model and `author` column to Note model

--- migrations/20170224_174012_create_user.py
+++ migrations/20170224_174012_create_user.py
@@ -1,11 +1,7 @@
 import datetime
 
 import peewee
-
-import config
-import markdown
-
-database = peewee.Proxy()
+from playhouse import migrate as pmigrate
 
 class User(peewee.Model):
     username = peewee.CharField(max_length=64, unique=True)
@@ -13,24 +9,13 @@ class User(peewee.Model):
     salt = peewee.CharField(max_length=32)
     creation_time = peewee.DateTimeField(default=datetime.datetime.now)
 
-    class Meta:
-        database = database
-
 class Note(peewee.Model):
     author = peewee.ForeignKeyField(User, related_name='notes')
     text = peewee.TextField(verbose_name="Text")
     creation_time = peewee.DateTimeField(default=datetime.datetime.now)
 
-    def render(self):
-        return markdown.render(self.text)
-
-    class Meta:
-        database = database
-
-def connect():
-    if config.DATABASE['BACKEND'] == "sqlite":
-        db = peewee.SqliteDatabase(config.DATABASE['NAME'])
-    else:
-        raise ValueError('undefined DB backend: {}'.format(config.DATABASE['BACKEND']))
-
-    database.initialize(db)
+def migrate(database, migrator):
+    database.create_table(User)
+    # We have to recreate model, because we added non-nullable foreign key (author)
+    database.drop_table(Note)
+    database.create_table(Note)
