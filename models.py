aca0481c1d45c5344339abafae538fedc67f947e
Oleg Yamnikov <bgeyts667@gmail.com>
2017-02-24 18:02:59 +0500

    Add password management methods to User model

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 import datetime
+import hashlib
+import random
+import string
 
 import peewee
 
@@ -7,11 +10,36 @@ import markdown
 
 database = peewee.Proxy()
 
+def hash_password(pwd, salt):
+    sha256 = hashlib.sha256()
+    sha256.update(pwd.encode('utf8'))
+    sha256.update(salt.encode('utf8'))
+    return sha256.hexdigest()
+
 class User(peewee.Model):
     username = peewee.CharField(max_length=64, unique=True)
     password = peewee.CharField(max_length=32)
     salt = peewee.CharField(max_length=32)
     creation_time = peewee.DateTimeField(default=datetime.datetime.now)
+
+    def reset_salt(self):
+        def random_char():
+            return random.choice(string.ascii_letters + string.digits)
+
+        self.salt = "".join([ random_char() for _ in range(32) ])
+        return self.salt
+
+    def set_password(self, password):
+        if not self.salt:
+            self.reset_salt()
+        self.password = hash_password(password, self.salt)
+
+    def check_password(self, password):
+        if not self.salt:
+            self.reset_salt()
+
+        hashed = hash_password(password, self.salt)
+        return hashed == self.password
 
     class Meta:
         database = database
