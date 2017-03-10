73c75ae38306910e9ab79f42459443eb99ca8f95
Oleg Yamnikov <bgeyts667@gmail.com>
2017-03-10 22:05:33 +0500

    :hammer: Rename api v1 endpoints names

--- web/api.py
+++ web/api.py
@@ -17,13 +17,13 @@ def serialize_note(note):
 apiv1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 @apiv1.route('/notes', methods=['GET'])
-def notes():
+def notes_index():
     notes = Note.select().order_by(Note.creation_time.desc())
     notes_data = [ serialize_note(n) for n in notes ]
     return jsonify(notes_data)
 
 @apiv1.route('/notes/update', methods=['POST'])
-def update():
+def notes_update():
     nid = request.form.get('id')
     if not nid:
         return jsonify_status(400, message="ID is not provided")
@@ -42,7 +42,7 @@ def update():
     return jsonify(serialize_note(note))
 
 @apiv1.route('/notes/create', methods=['POST'])
-def create():
+def notes_create():
     text = request.form.get('text')
     if text is None:
         return jsonify_status(400, message="Text is not provided")
@@ -51,7 +51,7 @@ def create():
     return jsonify(serialize_note(note))
 
 @apiv1.route('/notes/delete', methods=['POST'])
-def delete():
+def notes_delete():
     nid = request.form.get('id')
     if not nid:
         return jsonify_status(400, message="ID is not provided")
