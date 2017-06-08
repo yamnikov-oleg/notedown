907958239a6274103873d8278874c470d9091e9f
Oleg Yamnikov <bgeyts667@gmail.com>
2017-06-08 17:18:53 +0500

    :art: Port main.py cli onto google's python fire

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python3
-import sys
+import fire
 
 from notedown import web, migrations, models, config
 
 
-if __name__ == '__main__':
-    if config.DEBUG:
-        print("Debug mode is enabled")
+class NotedownCLI:
+    """CLI to manage Notedown distribution"""
 
-    if sys.argv[1] == "server":
+    def __init__(self):
+        if config.DEBUG:
+            print("Debug mode is enabled")
+
+    def _prepare_models(self):
         models.connect(**config.DATABASE)
-        web.app.run(host=config.HOST, port=config.PORT)
-    elif sys.argv[1] == "migrate":
-        models.connect(**config.DATABASE)
+
+    def server(self, host=None, port=None):
+        """Run local Notedown server"""
+        self._prepare_models()
+
+        if not host:
+            host = config.HOST
+
+        if not port:
+            port = config.PORT
+
+        web.app.run(host=host, port=port)
+
+    def migrate(self):
+        self._prepare_models()
         migrations.migrate_db()
-    elif sys.argv[1] == "createuser":
+
+    def createuser(self):
+        self._prepare_models()
+
         from getpass import getpass
-        models.connect(**config.DATABASE)
         user = models.User()
         user.username = input("Username: ")
         user.set_password(getpass("Password: "))
         user.save()
         print("User is created!")
-    elif sys.argv[1] == "test":
+
+    def test(self):
         print("Creating an in-memory test database...")
         models.connect(BACKEND="sqlite", NAME=":memory:")
         migrations.migrate_db()
@@ -33,3 +51,7 @@ if __name__ == '__main__':
             'notedown.web.tests',
         ]))
         unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    fire.Fire(NotedownCLI)
